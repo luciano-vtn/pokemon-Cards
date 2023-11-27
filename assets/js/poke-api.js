0c04080adb4cbc5e08da88ee9e7fe8fcbdbe970a
unknown <luciano.vtn120@gmail.com>
2023-11-27 10:47:47 -0300

    modificações no projeto original

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -17,19 +17,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
-pokeApi.getPokemonDetail = async (pokemon) => {
-    const response = await fetch(pokemon.url)
-    const pokeDetail = await response.json()
-    return convertPokeApiDetailToPokemon(pokeDetail)
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    const response = await fetch(url)
-    const jsonBody = await response.json()
-    const pokemons = jsonBody.results
-    const detailRequests = pokemons.map(pokeApi.getPokemonDetail)
-    const pokemonsDetails = await Promise.all(detailRequests)
-    return pokemonsDetails
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
